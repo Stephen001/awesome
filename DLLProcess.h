a060f704c41ec092b78f00109339ee58943693eb
Stephen Badger <stephen.badger@gmail.com>
2010-06-12 16:31:35 +0100

    Modifies project to include Windows/Linux Eclipse build configurations. Updates DLL_IMPORT definition to cope with non-Windows systems as well. Minor edit to extend the array size in MathOps opgroup.

--- DLLProcess.h
+++ DLLProcess.h
@@ -8,10 +8,14 @@
 #ifndef DLLPROCESS_H_
 #define DLLPROCESS_H_
 
-#ifdef AWESOME_DLL_IMPORT
-#define DLL_DECL __declspec(dllimport)
+#ifdef _WIN32
+	#ifdef AWESOME_DLL_IMPORT
+		#define DLL_DECL __declspec(dllimport)
+	#else
+		#define DLL_DECL __declspec(dllexport)
+	#endif
 #else
-#define DLL_DECL __declspec(dllexport)
+#define DLL_DECL
 #endif
 
 #endif /* DLLPROCESS_H_ */
